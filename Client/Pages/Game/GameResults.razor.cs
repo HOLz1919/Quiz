ba8444c0193f6cf628e409987707f5be05ad778e
Mateusz Harasimowicz <matirati@gmail.com>
2021-05-30 21:19:45 +0200

    Result game View

---
+++ Client/Pages/Game/GameResults.razor.cs
@@ -0,0 +1,43 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Quiz.Client.Services;
+using Quiz.Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quiz.Client.Pages.Game
+{
+    public partial class GameResults
+    {
+        [Parameter]
+        public Guid MatchId { get; set; }
+
+
+        public List<UserMatchView> Scores = new List<UserMatchView>();
+        private string UserId { get; set; }
+
+
+        [Inject]
+        public IGameService GameService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        [Inject]
+        public ILocalStorageService _localStorage { get; set; }
+
+
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            UserId = await _localStorage.GetItemAsync<string>("UserId");
+            Scores = await GameService.GetResults(MatchId);
+            await base.OnInitializedAsync();
+        }
+
+
+
+    }
+}
